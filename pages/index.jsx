267bacae5fbdeb263da09861c2ca26e7b3513cf6
joseriosjose <jose_rios456@hotmail.com>
2022-05-18 16:54:44 -0500

    feat(base): base of proyect

---
+++ pages/index.jsx
@@ -0,0 +1,29 @@
+import Head from "next/head"; 
+import styles from "../styles/Home.module.css";
+
+/**es obligatorio que las paginas sean exportaciones por defecto */
+/**el nombre del archivo con el de la clase pueden no ser el mismo */
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home - Jose Rios</title>
+        <meta name="description" content="Home Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          ir a <a href="/about">About!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{" "}
+          <code className={styles.code}>pages/index.jsx</code>
+        </p>
+
+      </main>
+
+    </div>
+  );
+}
