72f9e2193fd62a460563c0709e2f8c3bd7a5dfd5
joseriosjose <jose_rios456@hotmail.com>
2022-05-19 11:25:47 -0500

    feat(components): NavBar add Styles

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 
+import Link from "next/link";
+import styles from "./Navbar.module.css";
 
 export const Navbar = () => {
   return (
-    <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">contact</a>
+    <nav className={styles["menu-container"]}>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/contact">contact</Link>
     </nav>
-  )
-}
+  );
+};
