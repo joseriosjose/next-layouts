d8ee4b3aada99d4fecf20a12046ab0d24c145fff
joseriosjose <jose_rios456@hotmail.com>
2022-05-23 16:16:39 -0500

    fix(ModuleCss): fix imports css global

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,20 +1,18 @@
 import Link from "next/link";
 import { MainLayout } from "../../components/layout/MainLayout";
-import styles from "../../styles/Home.module.css";
 
 /**se puede tener dos Head puesto a que cada uno sobrescribe el objeto head */
 export default function contact() {
   return (
     <MainLayout>
       <h1>Contact page</h1>
-      <h1 className={styles.title}>
+      <h1 className={"title"}>
         {/*ir a <a href="/.">Home!</a>*/}
         Ir a <Link href={"/"}>Home</Link>
       </h1>
 
-      <p className={styles.description}>
-        Get started by editing{" "}
-        <code className={styles.code}>pages/contact.jsx</code>
+      <p className={"description"}>
+        Get started by editing <code className={"code"}>pages/contact.jsx</code>
       </p>
     </MainLayout>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { MainLayout } from "../components/layout/MainLayout";
-import styles from "../styles/Home.module.css";
 
 /**es obligatorio que las paginas sean exportaciones por defecto */
 /**el nombre del archivo con el de la clase pueden no ser el mismo */
@@ -8,13 +7,12 @@ export default function Home() {
   return (
     <MainLayout>
       <h1>Home page</h1>
-      <h1 className={styles.title}>
+      <h1 className={"title"}>
         {/*ir a <a href="/about">About!</a>*/}
         Ir a <Link href={"/about"}>About</Link>
       </h1>
-      <p className={styles.description}>
-        Get started by editing{" "}
-        <code className={styles.code}>pages/index.jsx</code>
+      <p className={"description"}>
+        Get started by editing <code className={"code"}>pages/index.jsx</code>
       </p>
     </MainLayout>
   );
